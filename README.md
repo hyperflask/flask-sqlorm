b294dda4265511e6e45e1eb1a7d93c104aafb23d
Maxime Bouroumeau-Fuseau <maxime.bouroumeau@gmail.com>
2025-09-23 23:59:35 +0200

    Session is now managed at the app context level. Integration with EngineDispatcher

--- README.md
+++ README.md
@@ -31,21 +31,28 @@ class Task(db.Model):
     done: bool = db.Column(default=False)
 ```
 
-Start a transaction using the db object:
+A session is automatically started everytime an app context is created. Perform queries directly in your endpoints:
 
 ```python
-with db:
-    Task.create(title="first task")
+@app.route("/tasks")
+def list_tasks():
+    tasks = Task.find_all()
+    return render_template("tasks.html", tasks=tasks)
 ```
 
-In views, the current session is available using `db.session`
+The session is rollbacked at the end of the request.
 
-## Configuration
+To commit some data, start a transaction using the db object:
 
-Configure the sqlorm engine using the extension's constructor or `init_app()`. Configuration of the engine is performed using the URI method.
-Additional engine parameters can be provided as keyword arguments.
+```python
+@app.route("/tasks", methods=["POST"])
+def create_task():
+    with db:
+        task = Task.create(title=request.form["title"])
+    return render_template("task.html", task=task)
+```
 
-Configuration can also be provided via the app config under the `SQLORM_` namespace. Use `SQLORM_URI` to define the database URI.
+The current session is available using `db.session`
 
 ## Additional utilities provided by Flask-SQLORM
 
@@ -57,3 +64,60 @@ Model classes have the additional methods:
 ## Managing the schema
 
 Some CLI commands are available under the *db* command group. Check out `flask db --help` for a list of subcommands.
+
+## Configuration
+
+Configure the sqlorm engine using the extension's constructor or `init_app()`. Configuration of the engine is performed using the URI method.
+Additional engine parameters can be provided as keyword arguments.
+
+Configuration can also be provided via the app config under the `SQLORM_` namespace. Use `SQLORM_URI` to define the database URI.
+
+## Using multiple engines
+
+You can setup multiple engines via the config and use an [`EngineDispatcher`](https://hyperflask.github.io/sqlorm/engine/#engine-dispatcher) to select an engine to use.
+
+```py
+db = FlaskSQLORM(app, "sqlite://:memory:", alt_engines=[{"uri": "sqlite://:memory", "tags": ["readonly"]}])
+
+with db.engines.readonly:
+    # Execute on an engine randomly selected from the one matching the readonly tag
+
+with db.engines:
+    # Uses the default engine
+```
+
+The context can be used inside other contexts:
+
+```py
+@app.route()
+def endpoint():
+    objs = MyModel.find_all() # uses the default engine (in a non commit transaction)
+
+    with db.engines.master: # uses a random engine matching the master tag (in a committed transaction)
+        obj = MyModel.create()
+
+    with db.engines.readonly.session(): # uses a random engine matching the readonly tag (in a non commit transaction)
+        objs = MyModel.find_all()
+```
+
+You can create more advanced use case by subclassing `EngineDispatcher`. For example, to implement selection based on an http header that can be set by a load balancer to use the closest geographic replica. And use the primary server for write.
+
+```py
+from sqlorm import EngineDispatcher
+
+class HeaderEngineDispatcher(EngineDispatcher):
+    def select_all(self, tag=None):
+        if not tag and self.header and has_request_context() and self.header in request.headers:
+            return self.select_all(request.headers[self.header])
+        return super().select_all(tag)
+
+db = FlaskSQLORM(app, "postgresql://primary", engine_dispatcher_class=HeaderEngineDispatcher,
+                 alt_engines=[{"uri": "postresql://replica1", "tags": ["usa"]},
+                              {"uri": "postresql://replica2", "tags": ["europe"]}])
+
+@app.route()
+def endpoint():
+    MyModel.find_all() # execute on engine selected via header
+    with db:
+        MyModel.create() # execute on default engine
+```
--- src/flask_sqlorm/__init__.py
+++ src/flask_sqlorm/__init__.py
@@ -1,11 +1,12 @@
-from sqlorm import Engine, Model as _Model, get_current_session, init_db, migrate, create_all, PrimaryKey as PrimaryKey
+from sqlorm import Engine, EngineDispatcher, Model as _Model, get_current_session, init_db, migrate, create_all, PrimaryKey as PrimaryKey
 from sqlorm.engine import session_context
 from sqlorm.schema import create_initial_migration, set_schema_version
 import sqlorm
 import abc
 import os
 import click
-from flask import g, abort, has_request_context
+from flask import g, abort
+from flask.signals import appcontext_pushed, appcontext_tearing_down
 from flask.cli import AppGroup
 from werkzeug.local import LocalProxy
 
@@ -16,6 +17,9 @@ except ImportError:
     Encrypted = None
 
 
+session = LocalProxy(get_current_session)
+
+
 class FlaskSQLORM:
     def __init__(self, app=None, *args, **kwargs):
         for key in dir(sqlorm):
@@ -23,36 +27,46 @@ class FlaskSQLORM:
                 setattr(self, key, getattr(sqlorm, key))
         self.Model = Model
         self.Encrypted = Encrypted
+        self.session = session
         if app:
             self.init_app(app, *args, **kwargs)
 
-    def init_app(self, app, database_uri="sqlite://:memory:", migrations_folder="migrations", **engine_kwargs):
+    def init_app(self, app, database_uri="sqlite://:memory:", migrations_folder="migrations", alt_engines=None,
+                 engine_dispatcher_class=EngineDispatcher, **engine_kwargs):
         self.app = app
 
         config = app.config.get_namespace("SQLORM_")
+        self.migrations_folder = os.path.join(app.root_path, config.pop("migrations_folder", migrations_folder))
+        alt_engines = config.pop("engines", alt_engines or {})
+
         database_uri = config.pop("uri", database_uri)
-        migrations_folder = config.pop("migrations_folder", migrations_folder)
         for key, value in engine_kwargs.items():
-            config.setdefault(key, value)
-        config.setdefault("logger", app.logger)
-        if database_uri.startswith("sqlite://"):
-            config.setdefault("fine_tune", True)
-            config.setdefault("foreign_keys", True)
+            config.setdefault(key, value)   
+        self.engine = self.create_engine(database_uri, **config)
 
-        self.engine = Engine.from_uri(database_uri, **config)
-        self.session = LocalProxy(get_current_session)
-        self.migrations_folder = os.path.join(app.root_path, migrations_folder)
+        self.engines = engine_dispatcher_class()
+        self.engines.register(self.engine, default=True)
+        for params in alt_engines.items():
+            if isinstance(params, Engine):
+                self.engines.register(params)
+                continue
+            if isinstance(params, (list, tuple)):
+                self.engines.register(*params)
+                continue
+            engine_kwargs = dict(params)
+            tags = engine_kwargs.pop("tags", [])
+            engine = self.create_engine(engine_kwargs.pop("uri"), **engine_kwargs)
+            self.engines.register(engine, tags)
 
-        @app.before_request
-        def start_db_session():
-            g.sqlorm_session = self.engine.make_session()
+        def on_appcontext_pushed(app, **kw):
+            g.sqlorm_session = self.engines.select().make_session()
             session_context.push(g.sqlorm_session)
+        appcontext_pushed.connect(on_appcontext_pushed, weak=False)
 
-        @app.after_request
-        def close_db_session(response):
+        def on_appcontext_tearing_down(app, **kw):
             session_context.pop()
             g.sqlorm_session.close()
-            return response
+        appcontext_tearing_down.connect(on_appcontext_tearing_down, weak=False)
         
         cli = AppGroup("db", help="Commands to manage your database")
 
@@ -62,11 +76,13 @@ class FlaskSQLORM:
             self.init_db()
 
         @cli.command()
-        @click.option("--version", default="000")
+        @click.argument("models", nargs=-1)
+        @click.option("--version", default=None)
         @click.option("--set-version", is_flag=True)
-        def init_migrations(version, set_version):
+        def init_migrations(models, version, set_version):
             """Initializes migrations"""
-            self.init_migrations(version=version, set_version=set_version)
+            header, version, name = self.init_migrations(models=models, version=version, set_version=set_version)
+            click.echo(f"Created migration: {version}_{name}.sql ({header})")
 
         @cli.command()
         def create_all():
@@ -84,16 +100,18 @@ class FlaskSQLORM:
 
         app.cli.add_command(cli)
 
+    def create_engine(self, database_uri, **engine_kwargs):
+        engine_kwargs.setdefault("logger", self.app.logger)
+        if database_uri.startswith("sqlite://"):
+            engine_kwargs.setdefault("fine_tune", True)
+            engine_kwargs.setdefault("foreign_keys", True)
+        return Engine.from_uri(database_uri, **engine_kwargs)
+
     def __enter__(self):
-        if has_request_context():
-            return g.sqlorm_session.__enter__()
         return self.engine.__enter__()
 
     def __exit__(self, exc_type, exc_value, exc_tb):
-        if has_request_context():
-            g.sqlorm_session.__exit__(exc_type, exc_value, exc_tb)
-        else:
-            self.engine.__exit__(exc_type, exc_value, exc_tb)
+        self.engine.__exit__(exc_type, exc_value, exc_tb)
 
     def create_all(self, **kwargs):
         kwargs.setdefault("model_registry", self.Model.__model_registry__)
@@ -107,19 +125,22 @@ class FlaskSQLORM:
         with self.app.app_context(), self.engine:
             init_db(**kwargs)
 
-    def init_migrations(self, version="000", set_version=False):
+    def init_migrations(self, models=None, version=None, set_version=False, **kwargs):
         os.makedirs(self.migrations_folder, exist_ok=True)
-        create_initial_migration(path=self.migrations_folder, version=version)
+        kwargs.setdefault("path", self.migrations_folder)
+        kwargs.setdefault("model_registry", self.Model.__model_registry__)
+        header, version, name = create_initial_migration(version=version, models=models, **kwargs)
         if set_version:
             with self.engine:
                 set_schema_version(version)
+        return (header, version, name)
 
     def migrate(self, **kwargs):
         kwargs.setdefault("path", self.migrations_folder)
         kwargs.setdefault("logger", self.app.logger)
         with self.engine:
             migrate(**kwargs)
-    
+
 
 class Model(_Model, abc.ABC):
     @classmethod
